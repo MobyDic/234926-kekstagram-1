bbda95f9ca03950772db592af0ca1d990a1ab515
Yaroslav Makarenko <mobydic@mail.ru>
2018-02-20 02:19:18 +0300

    Добавлено сохранение в файл

--- src/greeting.js
+++ src/greeting.js
@@ -1,9 +1,61 @@
 require(`colors`);
+const readline = require(`readline`);
+const fs = require(`fs`);
+const generate = require(`./generate`);
+
 
 const packageInfo = require(`../package.json`);
 
 module.exports = {
   execute() {
     console.log(`Привет пользователь! Эта программа будет запускать сервер «Кекстаграм». Автор:${packageInfo.author}`.green);
+
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    const askForGenerate = () => new Promise((resolve, reject) => {
+      rl.question(`Cгенерировать данные (y/n)?`, (answer) => answer === `y` ? resolve() : reject());
+    });
+
+    const askForElements = () => new Promise((resolve) => {
+      rl.question(`Сколько элементов необходимо создать? `, (elements) => {
+        if (!isNaN(elements)) {
+          resolve(elements);
+        } else {
+          console.log(`Ошибка в числе`);
+          rl.close();
+        }
+      });
+    });
+
+    const askForPath = (elements) => new Promise((resolve) => {
+      rl.question(`Укажите путь до файла: `, (path) => resolve([elements, path]));
+    });
+
+    const askForExistingFile = (data) => new Promise((resolve) => {
+      fs.exists(data[1], (exists) => exists
+        ? rl.question(`Файл уже существует, перезаписать? (y/n) `, (answer) => answer === `y` ? resolve(data) : rl.close())
+        : resolve(data)
+      );
+    });
+
+    askForGenerate()
+        .then(askForElements)
+        .then(askForPath)
+        .then(askForExistingFile)
+        .then((result) => {
+          generate.execute(result);
+          console.log(`Файл создан!`);
+          return result;
+        })
+        .catch((err) => {
+          if (err) {
+            console.log(err);
+          }
+          return (err);
+        })
+        .then(() => rl.close());
   }
 };
---
+++ test/generate.test.js
@@ -0,0 +1,25 @@
+const assert = require(`assert`);
+const generate = require(`../src/generate`);
+const fs = require(`fs`);
+const {promisify} = require(`util`);
+const access = promisify(fs.access);
+const unlink = promisify(fs.unlink);
+
+describe(`Generate JSON file`, function () {
+  it(`should fail on not existing folder`, function () {
+    const tempFileName = `folder/testfile.json`;
+    const count = 1;
+    return generate.execute([count, tempFileName])
+        .then(() => assert.fail(`Path ${tempFileName} should not be available`))
+        .catch((e) => assert.ok(e));
+  });
+
+  it(`should create new file`, function () {
+    const tmpFileName = `test/testfile.json`;
+    const count = 2;
+    return generate.execute([count, tmpFileName])
+        .then(() => access(tmpFileName))
+        .then(() => unlink(tmpFileName));
+  });
+
+});
